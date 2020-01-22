b2e9ddc244092831d371c7754d44f125d176dae4
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-01-22 01:06:02 -0500

    minor change to seed data

--- data/seeds/cars.js
+++ data/seeds/cars.js
@@ -3,28 +3,24 @@ exports.seed = knex => {
     	.then(() => {
 			return knex('cars').insert([
 				{
-					id: 1,
 					vin: '12345',
 					make: 'toyota',
 					model: 'corolla',
 					mileage: 12345
 				},
 				{
-					id: 2,
 					vin: '12345',
 					make: 'honda',
 					model: 'accord',
 					mileage: 12345
 				},
 				{
-					id: 3,
 					vin: '12345',
 					make: 'toyota',
 					model: 'corolla',
 					mileage: 12345
 				},
 				{
-					id: 4,
 					vin: '12345',
 					make: 'toyota',
 					model: 'corolla',
